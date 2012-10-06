176529a4562bdeed72baf4ad1591c80ce8ed6ae9
Vasiliev Nikita <sharpyfox@gmail.com>
2012-10-06 19:59:34 +0400

    add some manuals_controller tests cases
    correct manual root after create
    correct root recreation on error
    correct manual_chapters destroy after manual destroy

--- test/functional/manuals_controller_test.rb
+++ test/functional/manuals_controller_test.rb
@@ -27,7 +27,14 @@ class ManualsControllerTest < ActionController::TestCase
 	def test_create_valid
 		Manual.any_instance.stubs(:valid?).returns(true)
 		post :create
+		man = assigns(:manual)
+		assert_equal 1, man.manual_chapters.count
 		assert_redirected_to manual_url(assigns(:manual))
+	end
+
+	def test_root_chapter_recreate		
+		man = manuals(:project_without_chapters)
+		assert man.root != nil		
 	end
   
 	def test_edit
@@ -49,8 +56,10 @@ class ManualsControllerTest < ActionController::TestCase
   
 	def test_destroy
 		manual = Manual.first
+		assert_equal 1, ManualChapter.find_all_by_manual_id(manual.id).count
 		delete :destroy, :id => manual
 		assert_redirected_to manuals_url
 		assert !Manual.exists?(manual.id)
+		assert_equal 0, ManualChapter.find_all_by_manual_id(manual.id).count
 	end
 end
