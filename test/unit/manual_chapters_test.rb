d436639ec8ccb7ccc212160e2ec3d2e651cec2e3
Vasiliev Nikita <sharpyfox@gmail.com>
2012-10-07 16:47:06 +0400

    unit tests for manuals model
    delete example_test cases

--- test/unit/manual_chapters_test.rb
+++ test/unit/manual_chapters_test.rb
@@ -2,8 +2,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ManualChapterTest < ActiveSupport::TestCase
   fixtures :manual_chapters
-
-  # Replace this with your real tests.
+  
   def test_truth
     assert true
   end
--- test/unit/manuals_test.rb
+++ test/unit/manuals_test.rb
@@ -3,8 +3,20 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ManualTest < ActiveSupport::TestCase
   fixtures :manuals
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_title_validate
+    assert_equal false, Manual.new(:title => nil).valid?
+    assert_equal true, Manual.new(:title => "some title").valid?
+  end
+
+  def test_auto_root_create
+    Manual.any_instance.stubs(:valid?).returns(true)
+    man = Manual.new(:title => "some title")    
+    assert man.save!
+    assert man.root # root node should be auto created
+  end
+
+  def test_auto_root_recreate
+	man = manuals(:project_without_chapters)   
+    assert man.root # if some goin wrong root node must be auto recreated
   end
 end
