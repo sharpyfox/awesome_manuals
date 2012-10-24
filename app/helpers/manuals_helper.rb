de41ff93bf3a36b4ea1afd984cca1bcf99986780
Vasiliev Nikita <sharpyfox@gmail.com>
2012-10-24 20:37:10 +0400

    new functions in helpers

--- app/helpers/manuals_helper.rb
+++ app/helpers/manuals_helper.rb
@@ -2,18 +2,56 @@ require 'forwardable'
 require 'cgi'
 
 module ManualsHelper
+  include ApplicationHelper
   include ManualChaptersHelper
 
-  def getContent(aPageId, aLevel)
-    zResult = render(:partial => "wiki/content", :locals => {:content => @wiki_pages = WikiPage.find_by_id(aPageId).content_for_version(nil)})
-    zResult = zResult.gsub(/(<a href="#).*(<\/a>)/){""}    
-    zResult = zResult.gsub(/h4/){"h#{(aLevel + 4).to_s()}"}
-    zResult = zResult.gsub(/h3/){"h#{(aLevel + 3).to_s()}"}
-    zResult = zResult.gsub(/h2/){"h#{(aLevel + 2).to_s()}"}
-    zResult = zResult.gsub(/h1/){"h#{(aLevel + 1).to_s()}"}
-    zResult = zResult.gsub(/(<ul class="toc).*(<\/ul>)/){""}    
-    zResult = zResult.gsub(/(src|href)="\//) { |s| "#{$1}=\"hthhtp://#{request.host_with_port}/" }
-    zResult
+  def getAttachment(aId)    
+    zReturn = "data:none;base64,=="
+    #return zReturn
+    zAtt = Attachment.find_by_id(aId)
+    if zAtt
+      content_type = zAtt.content_type
+      if content_type.blank?
+        content_type = Redmine::MimeType.of(zAtt.filename)
+      end
+      
+      return zAtt.diskfile      
+    else
+      return zReturn
+    end
+  end
+
+  def replaceToc(aTemplate)
+    aTemplate.gsub(/(<ul class="toc).*(<\/ul>)/){""}    
+  end
+
+  def replaceHeaders(aTemplate, aOffset, aIndex)
+    zResult = aTemplate.gsub(/h4/){"h#{(aOffset + aIndex + 3).to_s()}"}
+    zResult = zResult.gsub(/h3/){"h#{(aOffset + aIndex + 2).to_s()}"}
+    zResult = zResult.gsub(/h2/){"h#{(aOffset + aIndex + 1).to_s()}"}
+    return zResult.gsub(/h1/){"h#{(aOffset + aIndex).to_s()}"}
+  end
+
+  def replaceParagraphs(aTemplate)
+    aTemplate.gsub(/<a.*?href=.*?\s*?.*?>&para;<\/a>/, "")
+  end
+
+  def replacePictures(aTemplate)    
+    return aTemplate.gsub(/(<img.*?src=")\/.*?(\d*?)"/) { |s| "#{$1}#{getAttachment($2)}\"" }
+  end
+
+  def getContent(aChapter)    
+    zWikiPage = WikiPage.find_by_id(aChapter.wiki_page_id)
+    if zWikiPage      
+      zResult = render(:partial => "wiki/content", :locals => {:content => @wiki_pages = WikiPage.find_by_id(aChapter.wiki_page_id).content_for_version(nil)})
+      zResult = replaceParagraphs(zResult)
+      zResult = replaceHeaders(zResult, aChapter.level, aChapter.use_custom_title ? 1 : 0)
+      zResult = replaceToc(zResult)
+      zResult = replacePictures(zResult)
+      zResult
+    else
+      ""
+    end
   end
 
   def manual_structure(objects, &block)
@@ -22,6 +60,7 @@ module ManualsHelper
     return '' if objects.size == 0
 
     output = ''
+
     path = [nil]
 
     objects.each_with_index do |o, i|      
@@ -30,21 +69,21 @@ module ManualsHelper
         if path.include?(o.parent_id)
           # Remove wrong wrong tailing paths elements
           while path.last != o.parent_id
-            path.pop
-            #output << '</li></ul>'
-          end
-          #output << '</li><li>'
+            path.pop            
+          end          
         else
-          path << o.parent_id
-          #output << '<ul><li>'
+          path << o.parent_id          
         end
-      elsif i != 0
-        #output << '</li><li>'
+      elsif i != 0        
       end
-      output <<  "<h#{o.level}>" + o.title + "</h#{o.level}>" + "<br><br>" + getContent(o.wiki_page_id, o.level)
+      if o.use_custom_title
+        output <<  "<div class=\"wiki\"><h#{o.level}>" + o.title + "</h#{o.level}></div>"
+      end
+      if o.wiki_page_id
+        output << getContent(o)
+      end
     end
-
-    #output << '</li></ul>' * path.length
+    
     output.html_safe
   end
 end
---
+++ test/functional/manuals_helper_test.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'manuals_helper'
+
+class ManualsHelperTest < ActionController::TestCase
+	include ManualsHelper
+
+	PARAGRAPH_WITH_PREFIX = '<div><a some_attr="as" href="simple_href">&para;</a></div>'
+	PARAGRAPH_STRING = '<div><a href="simple_href">&para;</a></div>'
+	PARAGRAPH_WITH_SUFFIX = '<div><a href="simple_href" link="some">&para;</a></div>'
+
+	def test_paragraphs
+		assert_equal '<div></div>', replaceParagraphs(PARAGRAPH_WITH_PREFIX)
+		assert_equal '<div></div>', replaceParagraphs(PARAGRAPH_STRING)
+		assert_equal '<div></div>', replaceParagraphs(PARAGRAPH_WITH_SUFFIX)
+	end
+
+	HEADER_STRING = '<div><h1>asd</h1><h2>asd</h2><h3>asd</h3><h4>asd</h4></div>'
+	OFFSET_HEADER_RESULT_STRING = '<div><h5>asd</h5><h6>asd</h6><h7>asd</h7><h8>asd</h8></div>'
+	BIG_OFFSET_HEADER_RESULT_STRING = '<div><h6>asd</h6><h7>asd</h7><h8>asd</h8><h9>asd</h9></div>'
+
+	def test_headers
+		assert_equal OFFSET_HEADER_RESULT_STRING, replaceHeaders(HEADER_STRING, 5, 0)
+		assert_equal BIG_OFFSET_HEADER_RESULT_STRING, replaceHeaders(HEADER_STRING, 5, 1)
+	end
+end
