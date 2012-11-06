495495c19b45ebe3adb7f0f759ddb38c43059bf9
Vasiliev Nikita <sharpyfox@gmail.com>
2012-11-06 10:15:03 +0400

    wkhtmltopdf 0.11.0 compatibility

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -12,25 +12,13 @@ class ManualsController < ApplicationController
 	def generate
 		@manual = Manual.find(params[:id])
 
+
         render :pdf => @manual.title, # pdf will download as {manual_title}.pdf
         	:layout => 'manual_pdf', # uses views/layouts/manual_pdf.erb
        	 	:show_as_html => params[:debug].present?, # renders html version if you set debug=true in URL
         	:wkhtmltopdf  => Setting.plugin_awesome_manuals["wkhtmltopdf_bin"], # path to binary
         	:header => { :right => '[page]' },        	
-        	:toc    => {
-                           :depth              => 10,
-                           :header_text        => "Table of content",
-                           :l1_font_size       => 14,
-                           :l2_font_size       => 13,
-                           :l3_font_size       => 12,
-                           :l4_font_size       => 12,
-                           :l5_font_size       => 12,
-                           :l6_font_size       => 12,
-                           :l7_font_size       => 12                           
-                           },
-            :disable_internal_links         => true,
-            :disable_external_links         => true
-        	#:extra => 'toc --xsl-style-sheet "C:\test.xsl"'
+        	:extra => " --header-right \"[page]\" toc --xsl-style-sheet \"#{Rails.root}/public/#{Engines.plugins[:awesome_manuals].public_asset_directory}/templates/toc_template.xsl\""
 	end
   
 	def new
